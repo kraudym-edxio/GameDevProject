b25b256bafa2d0e4af8fa747b6e918b3d2d12298
Bailey Chittle <bachittle@gmail.com>
2021-12-20 22:50:37 -0500

    player teams now fill in a fair manner

--- Assets/Scripts/CTFPlayerManager.cs
+++ Assets/Scripts/CTFPlayerManager.cs
@@ -10,8 +10,29 @@ public class CTFPlayerManager : MonoBehaviour
 
 
     void Start() {
-        int numChoice = Random.Range(0,2);
-        playerTeam = numChoice == 0 ? Team.Red : Team.Blue;
+        // check if teams are unbalanced: if so balance by choosing team with lowest player count
+        int redCount = 0;
+        int blueCount = 0;
+        for (int i = 0; i < ChosenTeams.Count; i++) {
+            switch(ChosenTeams[i]) {
+                case Team.Red:
+                    redCount++;
+                    break;
+                case Team.Blue:
+                    blueCount++;
+                    break;
+            }
+        }
+        // if balanced, choose randomly
+        if (redCount == blueCount) {
+            int numChoice = Random.Range(0,2);
+            playerTeam = numChoice == 0 ? Team.Red : Team.Blue;
+        } else if (redCount > blueCount) {
+            playerTeam = Team.Blue;
+        } else {
+            playerTeam = Team.Red;
+        }
+        ChosenTeams.Add(playerTeam);
 
         switch (playerTeam) {
             case Team.Red:
